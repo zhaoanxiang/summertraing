d53bc788b6bbb0a782578744d99324b6c1ace5c4
learn1 <52434266+zhaoanxiang@users.noreply.github.com>
2019-07-22 18:23:53 +0800

    C++代码

---
+++ 201907221.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "hello world"<<endl;
+	
+		int a = 1, b = 3;
+		int c = a + b;
+		cout <<"a+b="<<c << endl;
+		system("pause");
+		return 0;
+		
+
+}
